7a190edadb7c90ae635323cac3bc950ed55431a7
leonardo <leonardo@localhost.localdomain>
2023-12-02 20:23:24 -0300

    Acomodando rutas para el deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './routes/AppMain.jsx'
 import { ProgMain } from './routes/main-prog.jsx'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { createBrowserRouter,RouterProvider, Route } from 'react-router-dom'
 import './css/index.css'
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<App/>
+    path: "/",
+    element: <App />,
   },
   {
-    path:"/programacion",
-    element:<ProgMain/>
+        path: "/programacion",
+        element: <ProgMain />
   }
-])
+    ]
+  
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
