4b03ac8faf65d0988c91e6a07845e39456b313f1
leonardo <leonardo@localhost.localdomain>
2023-12-02 16:00:01 -0300

    mejorando las rutas html 6xdxd

--- src/main-prog.jsx
+++ src/main-prog.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import {AppProg} from './prog.jsx'
 
 const textoProg = "El técnico secundario en programación es un programa educativo orientado a estudiantes de nivel secundario que desean adquirir habilidades prácticas en el campo de la programación informática. Esta formación tiene como objetivo proporcionar a los estudiantes una base sólida en conceptos fundamentales de programación y la capacidad de desarrollar software básico."
-const urlProg = "/public/programacion.jpg"
+const urlProg = "/programacion.jpg"
 
 ReactDOM.createRoot(document.getElementById('rootProg')).render(
     <>
