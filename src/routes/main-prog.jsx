3da7ea05d6f2c8e7ce16416ea23879f710573b49
leonardo <martinezjleonardo@gmail.com>
2023-12-03 17:12:48 -0300

    Terminando sección de Programación. Componentes en prog.jsx que son modificables por parámetros y reutilizables en las otras orientaciones

--- src/routes/main-prog.jsx
+++ src/routes/main-prog.jsx
@@ -1,6 +1,6 @@
 import {AppProg} from '../main-components/prog.jsx'
 
-const textoProg = "El técnico secundario en programación es un programa educativo orientado a estudiantes de nivel secundario que desean adquirir habilidades prácticas en el campo de la programación informática. Esta formación tiene como objetivo proporcionar a los estudiantes una base sólida en conceptos fundamentales de programación y la capacidad de desarrollar software básico."
+const textoProg = "El técnico secundario en programación es un programa educativo orientado a estudiantes de nivel secundario que desean adquirir habilidades prácticas en el campo de la programación. Esta formación tiene como objetivo proporcionar a los estudiantes una base sólida en conceptos fundamentales de programación."
 const urlProg = "/programacion.jpg"
 
 const ProgMain = () => {
